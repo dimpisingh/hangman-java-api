5b40c98c0bafec1fc1ff47f8505f361af4eb394d
Pete Sousa <im.pete.sousa@gmail.com>
2019-08-12 00:05:02 -0300

    updated PlayerRepository

--- src/main/java/com/dell/hangman/domain/repository/PlayerRepository.java
+++ src/main/java/com/dell/hangman/domain/repository/PlayerRepository.java
@@ -2,6 +2,11 @@ package com.dell.hangman.domain.repository;
 
 import com.dell.hangman.domain.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    public Player findByUsername(String username);
+
 }
